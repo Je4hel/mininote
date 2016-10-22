81522d482a77a53e700d4553b3c5f291ef8e8d60
Guillaume Lecêtre <guillaume.lecetre@efrei.net>
2016-10-22 18:29:44 +0200

    New NavPanel component and stories

--- src/components/NavPanel.js
+++ src/components/NavPanel.js
@@ -1,18 +1,27 @@
 import React, { Component } from "react";
+import NavPanelItem from "./NavPanelItem";
+import "../resources/NavPanel.css";
 
 class NavPanel extends Component {
     render() {
-        console.log(this.props.items);
         return (
-            <div className="navpanel-wrapper">
-                <ul>
-                    {this.props.items.map((item) => {
-                        return <li>{item.name}</li>
-                    })}
-                </ul>
+            <div className="navpanel">
+                {this.props.items.map((item) => {
+                    return <NavPanelItem key={item.key} onClick={item.onClick} {...item} />
+                })}
             </div>
         )
     }
+}
+
+NavPanel.propTypes = {
+    items: React.PropTypes.arrayOf(React.PropTypes.shape({
+        key: React.PropTypes.any.isRequired,
+        title: React.PropTypes.string.isRequired,
+        text: React.PropTypes.string,
+        img: React.PropTypes.string,
+        separator: React.PropTypes.bool
+    }))
 }
 
 export default NavPanel;
--- src/components/stories.js
+++ src/components/stories.js
@@ -1,70 +1,76 @@
 import React from 'react';
 import { storiesOf, action, linkTo } from '@kadira/storybook';
-import Counter from './Counter';
+import "../../public/global.css";
 import TextEditor from "./TextEditor";
 import NavPanel from "./NavPanel";
-
-storiesOf('Counter', module)
-    .add('Default', () => (
-        <Counter />
-    ))
-    .add("With starting value", () => (
-        <Counter start={5} />
-    ));
+import NavPanelItem from "./NavPanelItem"
 
 storiesOf("TextEditor", module)
-    .add("Default - no props", () => (
+    .addDecorator((story) => (
         <div style={{
             width: "50%",
             margin: "auto",
-            marginTop: "50px"
+            marginTop: "50px",
+            height: "1000px",
+            backgroundImage: "url('https://static.vecteezy.com/system/resources/previews/000/104/103/original/retro-background-pattern-vector.jpg')"
         }}>
-            <TextEditor rows="20"/>
+            {story()}
         </div>
+    ))
+    .add("Default - no props", () => (
+        <TextEditor rows="20"/>
     ))
     .add("View mode - disallow edit", () => (
-        <div style={{
-            width: "50%",
-            margin: "auto",
-            marginTop: "50px"
-        }}>
-            <TextEditor
-                mode={TextEditor.mode.VIEW}
-                allowEdit={false}
-                content="Hello **darkness**, my old friend. I've come to *talk* with you again"/>
-        </div>
+        <TextEditor
+            mode={TextEditor.mode.VIEW}
+            allowEdit={false}
+            content="Hello **darkness**, my old friend. I've come to *talk* with you again"/>
     ))
     .add("View mode - allow edit", () => (
-        <div style={{
-            width: "50%",
-            margin: "auto",
-            marginTop: "50px"
-        }}>
-            <TextEditor
-                mode={TextEditor.mode.VIEW}
-                allowEdit={true}
-                content="Hello **darkness**, my old friend. I've come to *talk* with you again"/>
-        </div>
+        <TextEditor
+            mode={TextEditor.mode.VIEW}
+            allowEdit={true}
+            content="Hello **darkness**, my old friend. I've come to *talk* with you again"/>
     ))
     .add("Edit mode", () => (
-        <div style={{
-            width: "50%",
-            margin: "auto",
-            marginTop: "50px"
-        }}>
-            <TextEditor
-                mode={TextEditor.mode.EDIT}
-                content="Hello **darkness**, my old friend. I've come to *talk* with you again"/>
-        </div>
+        <TextEditor
+            mode={TextEditor.mode.EDIT}
+            content="Hello **darkness**, my old friend. I've come to *talk* with you again"/>
     ));
 
 storiesOf("NavPanel", module)
+    .addDecorator((story) => (
+        <div style={{
+            width: "50%",
+            margin: "auto",
+            marginTop: "50px",
+            height: "1000px",
+            backgroundImage: "url('https://static.vecteezy.com/system/resources/previews/000/104/103/original/retro-background-pattern-vector.jpg')"
+        }}>
+            {story()}
+        </div>
+    ))
     .add("Default", () => {
-        let items = [
-            {name: "Item 1"},
-            {name: "Item 2"},
-            {name: "Item 3"},
-            {name: "Item 4"}
-        ];
-        return <NavPanel items={items} />
-    });
+        let items = getPanelItems();
+        return (
+            <NavPanel items={items} />
+        );
+    })
+    .add("Item with round image", () => (
+        <NavPanelItem title="Mon item 1" text="updated yesterday" img="http://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/sign-check-icon.png"/>
+    ))
+    .add("Item with square image", () => (
+        <NavPanelItem title="Mon item 1" text="updated yesterday" img="http://thumb1.shutterstock.com/display_pic_with_logo/3759446/419286670/stock-vector-square-emoticons-avatars-and-icons-419286670.jpg"/>
+    ))
+    .add("Item without image", () => (
+        <NavPanelItem title="Mon item 1" text="updated yesterday" />
+    ));
+
+function getPanelItems() {
+    return [
+        {key: "item1", title: "Item 1", text: "updated last year", img: "http://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/sign-check-icon.png"},
+        {key: "item2", title: "Item 2", img: "http://thumb1.shutterstock.com/display_pic_with_logo/3759446/419286670/stock-vector-square-emoticons-avatars-and-icons-419286670.jpg"},
+        {key: "item3", title: "Item 3", text: "Other items", separator: true},
+        {key: "item4", title: "Item 4", onClick: action("Clicked")}
+    ];
+}
